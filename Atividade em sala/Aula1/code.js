094795c60454e901eef6454f3b34d393cce5e1f6
Guilherme Henrique <guilherme_he_nrrique@hotmail.com>
2023-08-07 10:45:23 -0300

    Aula 07/08/2023

---
+++ Atividade em sala/Aula1/code.js
@@ -0,0 +1,7 @@
+function mudarCores(){
+    title1.style.color = 'red'
+    //div1.style.background = 'gray'
+    div1.innerHTML = "<p> Clicou pq é gay </p>"
+}
+
+title1.addEventListener('click',mudarCores);
